3fd431dace5ce79a4f772e5ab53daa81bfe70a8f
rameel ahmed <rameelahmed786@gmail.com>
2025-07-15 09:30:38 -0400

    contact me form

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -1,2 +1,16 @@
 import streamlit as st 
 st.header("Contact Me")
+
+st.title("Streamlit Form Demo")
+st.subheader("Enter details below")
+
+# DOCUMENTATION >> st.help(st.form)
+
+# CREATING OUR FORM FIELDS
+with st.form("form1", clear_on_submit=True):
+    name = st.text_input("Enter full name")
+    email = st.text_input("Enter email")
+    message = st.text_area("Message")
+    
+
+    submit = st.form_submit_button("Submit this form")
